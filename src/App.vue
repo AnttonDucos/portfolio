5aa6e90ba048dc5ca67db380ce0091b776623549
Ducos <antton.ducos@epitech.eu>
2025-02-12 11:19:16 +0100

    feat: studies

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Header />
     <Projects />
     <Studies />
+    <Hobbies />
     <Contact />
   </div>
 </template>
@@ -10,7 +11,8 @@
 <script setup>
 import Header from "./components/Header.vue";
 import Projects from "./components/Projects.vue";
-import Studies from "./components/Hobbies.vue";
+import Studies from "./components/Studies.vue";
+import Hobbies from "./components/Hobbies.vue";
 import Contact from "./components/Contact.vue";
 import "./styles.css";
 </script>
---
+++ src/components/Studies.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="studies-section">
+    <h3>My Studies</h3>
+    <div v-for="(study, index) in studies" :key="index" class="study-box">
+      <img :src="study.logo" alt="Logo" class="study-logo" />
+      <div class="study-content">
+        <h4>{{ study.name }}</h4>
+        <p class="university">{{ study.university }}</p>
+        <p class="dates">{{ study.dates }}</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// Données des études
+import schoolLogo1 from "/epitech.png";
+import schoolLogo2 from "../assets/ub.jpg";
+
+const studies = ref([
+  {
+    name: "Licence Informatique",
+    university: "Epitech Bordeaux",
+    dates: "Septembre 2023 - Now",
+    logo: schoolLogo1,
+  },
+  {
+    name: "Bachelor's Degree in Pure Mathematics",
+    university: "Bordeaux University",
+    dates: "September 2019 - May 2023",
+    logo: schoolLogo2,
+  },
+]);
+</script>
